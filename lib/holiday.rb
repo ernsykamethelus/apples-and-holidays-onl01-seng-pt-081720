99bd14c9e9dbe785020ca38cafd7af587220d090
Ernsyka Methelus <ernsykamethelus@yahoo.com>
2020-08-30 19:48:16 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -29,8 +29,8 @@ end
 
 
 
-
-holiday_hash.each do |season, holidays|
+def all_supplies_in_holidays(holiday_hash)
+  holiday_hash.each do |season, holidays|
     puts "#{season.to_s.capitalize}:"
 
     holidays.each do |holiday, supplies|
